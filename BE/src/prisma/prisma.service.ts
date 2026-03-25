addf354924dcf78a0894a4838c5b293eed4d72ef
RizkiNoviann <rizkinovian236@gmail.com>
2026-03-25 12:01:22 +0800

    fix be

--- BE/src/prisma/prisma.service.ts
+++ BE/src/prisma/prisma.service.ts
@@ -3,6 +3,23 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const databaseUrl =
+      process.env.DATABASE_URL ?? process.env.PRISMA_DATABASE_URL ?? process.env.POSTGRES_URL;
+
+    super(
+      databaseUrl
+        ? {
+            datasources: {
+              db: {
+                url: databaseUrl,
+              },
+            },
+          }
+        : undefined,
+    );
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
